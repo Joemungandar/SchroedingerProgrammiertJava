ada7d8bd79d0ae8c64fb6416e42e3c186a69f9e0
Joemungandar <joemungandar95@gmail.com>
2016-07-05 19:10:28 +0200

    update HalloSchroedinger.java

--- src/de/galileocomputing/schroedinger/java/kapitel01/HalloSchroedinger.java
+++ src/de/galileocomputing/schroedinger/java/kapitel01/HalloSchroedinger.java
@@ -1,12 +1,17 @@
 package de.galileocomputing.schroedinger.java.kapitel01;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class HalloSchroedinger {
 
 	public static void main(String[] args) throws IOException {
-		System.out.println("Hallo " + "");
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Wie heiﬂt du? ");
+		System.out.print("Name: ");
+		String name = scanner.nextLine();
+		System.out.println("Hallo " + name);
 		System.out.println("Wie alt bist du?");
-		int alter = System.in.read();
+		int alter = scanner.nextInt();
 		System.out.println(alter);
 	}
 }
